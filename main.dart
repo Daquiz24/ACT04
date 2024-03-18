f6af97f0e40b0b4ddff6c32f4ae62bd32c1c2979
pipaolim <limcriss78@gmail.com>
2024-03-18 18:00:29 +0800

    Lim Criss Paolo ADDED* class

--- main.dart
+++ main.dart
@@ -9,3 +9,36 @@ class Loading extends StatefulWidget {
   @override
   State<Loading> createState() => _LoadingState();
 }
+
+class _LoadingState extends State<Loading> {
+  Future<void> getData() async {
+    try {
+      final response =
+      await http.get(Uri.parse(url)).timeout(Duration(seconds: 6));
+      print("connected");
+      data = [jsonDecode(response.body)];
+      print(data[0]['results'][0]['name']['first']);
+      await Future.delayed(Duration(seconds: 3));
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => Homepage()));
+    } catch (e) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Message'),
+            content: Text('No internet connection'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  getData();
+                  Navigator.pop(context);
+                },
+                child: Text('Retry'),
+              )
+            ],
+          );
+        },
+      );
+    }
+  }

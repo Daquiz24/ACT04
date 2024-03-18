a97de70b94c8f3fda7c14af63c14ee483fe2293f
pipaolim <limcriss78@gmail.com>
2024-03-18 18:35:12 +0800

    JAN HENLEY ADDED* SHADOW BOX

--- homepage.dart
+++ homepage.dart
@@ -150,5 +150,13 @@ class _HomepageState extends State<Homepage> {
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(20),
-          
+          boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
         
